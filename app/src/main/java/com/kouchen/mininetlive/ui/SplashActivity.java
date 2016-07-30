f6d0b2087ef03428770a6f7cc594c224f3c1775d
cainli <leelong1207@gmail.com>
2016-07-30 13:47:28 +0800

    splash time delay refactor to rxjava

--- app/src/main/java/com/kouchen/mininetlive/ui/SplashActivity.java
+++ app/src/main/java/com/kouchen/mininetlive/ui/SplashActivity.java
@@ -1,10 +1,17 @@
 package com.kouchen.mininetlive.ui;
 
 import android.os.Bundle;
+
 import com.kouchen.mininetlive.R;
 import com.kouchen.mininetlive.ui.base.BaseActivity;
+
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 public class SplashActivity extends BaseActivity {
 
@@ -12,11 +19,13 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                finish();
-            }
-        }, 2000);
+        Observable.timer(2000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        finish();
+                    }
+                });
     }
 }
